f210ac65706eb93de28065c594bb4295a224d435
webAine <a.isaevew@gmail.com>
2024-12-11 23:44:09 +0100

    about + animated title

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import About from "./components/About";
 import Hero from "./components/Hero";
 
 const App = () => {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
       <Hero />
-      <section className="z-0 min-h-screen bg-blue-500"></section>
+      <About />
     </main>
   );
 };
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,7 +13,7 @@ const Hero = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [loadedVideos, setLoadedVideos] = useState(0);
 
-  const totalVideos = 3;
+  const totalVideos = 4;
   const nextVideoRef = useRef(null);
 
   const upcomingVideoIndex = (currentIndex % totalVideos) + 1;
