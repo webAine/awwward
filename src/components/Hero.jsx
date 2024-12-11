5db5463a1b27dfd5575a180851fdc41388f131d2
webAine <a.isaevew@gmail.com>
2024-12-12 00:59:42 +0100

    fixed hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -94,11 +94,11 @@ const Hero = () => {
       )}
 
       <div
-        className="relative z-10 h-dvh w-screen overflow-hidden bg-blue-75"
         id="video-frame"
+        className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75"
       >
         <div>
-          <div className="mask-clip-path absolute-center size-64 z-50 cursor-pointer overflow-hidden rounded-lg">
+          <div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
             <div
               className="origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100"
               onClick={handleMiniVdClick}
@@ -138,7 +138,7 @@ const Hero = () => {
           G<b>a</b>ming
         </h1>
         <div className="absolute left-0 top-0 z-40 size-full">
-          <div className="mt24 px-5 sm:px-10">
+          <div className="mt-24 px-5 sm:px-10">
             <h1 className="special-font hero-heading text-blue-100">
               redefi<b>n</b>e
             </h1>
